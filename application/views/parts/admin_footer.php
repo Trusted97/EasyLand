a5379b0840f665597e5cf0a24d65e82c9f40613f
Trusted97 <gianlucabenucci9700@gmail.com>
2019-12-30 14:11:07 +0100

    -Edit admin footer

--- application/views/parts/admin_footer.php
+++ application/views/parts/admin_footer.php
@@ -1,5 +1,5 @@
 <footer class="container py-5 text-center text-small">
-  <p class="mb-1">&copy; 2019 ScopriSpazio | Tutti i diritti riservati</p>
+  <p class="mb-1">&copy; <?php echo date('Y'); ?> EasyLand | Tutti i diritti riservati</p>
   <ul class="list-inline">
     <li class="list-inline-item"><a href="#">Privacy</a></li>
     <li class="list-inline-item"><a href="#">Cookie</a></li>
